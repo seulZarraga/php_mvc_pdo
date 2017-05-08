6dfe7bcde627d20420f6a947725cfcca095371c2
Seul Zarraga Espinosa <seulzarragaespinosa@Seuls-MacBook-Pro.local>
2017-05-07 19:05:24 -0500

    ajax

---
+++ 06.AJAX/views/modules/ajax.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once "../../controllers/controller.php";
+require_once "../../models/crud.php";
+
+class Ajax{
+
+	public $validarUsuario;
+	public $validarEmail;
+
+	public function validarUsuarioAjax(){
+
+		$datos = $this->validarUsuario;
+
+		$respuesta = MvcController::validarUsuarioController($datos); 
+
+		echo $respuesta;
+
+	}
+
+	public function validarEmailAjax(){
+
+		$datos = $this->validarEmail;
+
+		$respuesta = MvcController::validarEmailController($datos); 
+
+		echo $respuesta;
+
+	}
+
+}
+
+if(isset( $_POST["validarUsuario"])){
+	
+	$a = new Ajax();
+	$a -> validarUsuario = $_POST["validarUsuario"];
+	$a -> validarUsuarioAjax();
+
+}
+
+if(isset( $_POST["validarEmail"])){
+
+	$b = new Ajax();
+	$b -> validarEmail = $_POST["validarEmail"];
+	$b -> validarEmailAjax();
+
+}
+
